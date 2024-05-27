205144776085e3787468eb150056474b1127ca78
7244 <123005911+7244@users.noreply.github.com>
2024-05-27 18:58:46 +0300

    up

--- audio/SystemAudio/backend/xaudio2.h
+++ audio/SystemAudio/backend/xaudio2.h
@@ -5,17 +5,19 @@ system_audio_t *system_audio(){
 f32_t InternalVolume = 1;
 f32_t Volume = 1;
 
-TH_id_t thid;
-
 IXAudio2 *ctx = NULL;
 IXAudio2MasteringVoice *MasterVoice = NULL;
 IXAudio2SourceVoice *SourceVoice;
 
-static void *_thread_func(void *p) {
-  auto system_audio = (system_audio_t *)p;
-  auto This = &system_audio->Out;
+struct xacb_t : IXAudio2VoiceCallback{
+  void __stdcall OnStreamEnd() { }
 
-  while(1){
+  void __stdcall OnVoiceProcessingPassEnd() { }
+  void __stdcall OnVoiceProcessingPassStart(UINT32 SamplesRequired) { }
+  void __stdcall OnBufferEnd(void *p){
+    auto system_audio = (system_audio_t *)p;
+    auto This = &system_audio->Out;
+
     f32_t frames[_constants::CallFrameCount * _constants::ChannelAmount] = {0};
 
     system_audio->Process._DataCallback(frames);
@@ -30,43 +32,34 @@ static void *_thread_func(void *p) {
     xabuf.AudioBytes = _constants::CallFrameCount * _constants::ChannelAmount * sizeof(f32_t);
     xabuf.pAudioData = (uint8_t *)frames;
     xabuf.Flags = XAUDIO2_END_OF_STREAM;
-    // xabuf.pContext = (void *)This; TODO
-
-    while(1){
-      XAUDIO2_VOICE_STATE state;
-      This->SourceVoice->GetState(&state);
-      if (state.BuffersQueued < 2) {
-        break;
-      }
-      This->SourceVoice->FlushSourceBuffers();
-    }
+    xabuf.pContext = (void *)This;
 
     HRESULT hr = This->SourceVoice->SubmitSourceBuffer(&xabuf);
     if(FAILED(hr)){
       fan::throw_error("xaudio2", __LINE__);
     }
   }
-
-  return 0;
-}
+  void __stdcall OnBufferStart(void* pBufferContext) { }
+  void __stdcall OnLoopEnd(void* pBufferContext) { }
+  void __stdcall OnVoiceError(void* pBufferContext, HRESULT Error) { }
+};
 
 sint32_t Open(){
   HRESULT hr;
 
-
   hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
-  if (FAILED(hr)) {
-    fan::throw_error("xaudio2", __LINE__);
-    return 1;
-  }
-
-  hr = XAudio2Create(&ctx, 0, XAUDIO2_DEFAULT_PROCESSOR);
   if(FAILED(hr)){
     fan::throw_error("xaudio2", __LINE__);
     return 1;
   }
 
-  hr = ctx->CreateMasteringVoice(&MasterVoice);
+  hr = XAudio2Create(&This->ctx, 0, XAUDIO2_DEFAULT_PROCESSOR);
+  if(FAILED(hr)){
+    fan::throw_error("xaudio2", __LINE__);
+    return 1;
+  }
+
+  hr = This->ctx->CreateMasteringVoice(&This->MasterVoice);
   if(FAILED(hr)){
     fan::throw_error("xaudio2", __LINE__);
     return 1;
@@ -81,27 +74,46 @@ sint32_t Open(){
   waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;
   waveFormat.cbSize = 0;
 
-  hr = ctx->CreateSourceVoice(
-    &SourceVoice,
-    &waveFormat
+  xacb_t xacb;
+  hr = This->ctx->CreateSourceVoice(
+    &This->SourceVoice,
+    &waveFormat,
+    0,
+    XAUDIO2_DEFAULT_FREQ_RATIO,
+    &xacb
   );
   if(FAILED(hr)){
     fan::throw_error("xaudio2", __LINE__);
     return 1;
   }
 
-  SourceVoice->Start(0);
+  This->SourceVoice->Start(0);
 
-  this->thid = TH_open((void *)_thread_func, system_audio());
+  for(uint8_t i = 0; i < 2; i++){
+    f32_t frames[_constants::CallFrameCount * _constants::ChannelAmount] = {0};
+
+    XAUDIO2_BUFFER xabuf = {0};
+    xabuf.AudioBytes = _constants::CallFrameCount * _constants::ChannelAmount * sizeof(f32_t);
+    xabuf.pAudioData = (uint8_t *)frames;
+    xabuf.Flags = XAUDIO2_END_OF_STREAM;
+    xabuf.pContext = (void *)system_audio();
+
+    hr = This->SourceVoice->SubmitSourceBuffer(&xabuf);
+    if(FAILED(hr)){
+      fan::throw_error("xaudio2", __LINE__);
+    }
+  }
 
   return 0;
 }
 void Close(){
   __abort();
+  /* TODO
   this->SourceVoice->DestroyVoice();
   this->MasterVoice->DestroyVoice();
   this->ctx->Release();
   CoUninitialize();
+  */
 }
 
 void SetVolume(f32_t Volume) {
@@ -109,9 +121,9 @@ void SetVolume(f32_t Volume) {
   //__atomic_store(&this->Volume, &Volume, __ATOMIC_RELAXED);
 }
 f32_t GetVolume() {
-  f32_t r;
   fan::throw_error_impl();
- // __atomic_store(&r, &this->Volume, __ATOMIC_RELAXED);
+  f32_t r;
+  //__atomic_store(&r, &this->Volume, __ATOMIC_RELAXED);
   return r;
 }
 
